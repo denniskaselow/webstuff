fe4b207dff63349aca5b65c757f233a4868ab0b2
Dennis Kaselow <dennis.kaselow+github@gmail.com>
2016-08-18 19:06:06 +0200

    device orientation

--- web/main.dart
+++ web/main.dart
@@ -4,5 +4,28 @@
 import 'dart:html';
 
 void main() {
+  window.onDeviceOrientation.listen((event) {
+    querySelector('#alpha').text = '${event.alpha}';
+    querySelector('#beta').text = '${event.beta}';
+    querySelector('#gamma').text = '${event.gamma}';
+    querySelector('#absolute').text = '${event.absolute}';
+  });
 
+  window.onDeviceMotion.listen((event){
+    var acc = event.acceleration;
+    var accGrav = event.accelerationIncludingGravity;
+    var rotationRate = event.rotationRate;
+    var interval = event.interval;
+    querySelector('#x').text = '${acc.x}, grav: ${accGrav.x}';
+    querySelector('#y').text = '${acc.y}, grav: ${accGrav.y}';
+    querySelector('#z').text = '${acc.z}, grav: ${accGrav.z}';
+    querySelector('#rotalpha').text = '${rotationRate.alpha}';
+    querySelector('#rotbeta').text = '${rotationRate.beta}';
+    querySelector('#rotgamma').text = '${rotationRate.gamma}';
+    querySelector('#interval').text = '${interval}';
+  });
+
+  window.on['compassneedscalibration'].listen((event) {
+    querySelector('#info').text = 'Your compass needs calibrating! Wave your device in a figure-eight motion';
+  });
 }
