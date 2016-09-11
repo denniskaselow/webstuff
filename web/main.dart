47130e82d48e602da18e1ef78352839306060dbe
Dennis Kaselow <dennis.kaselow+github@gmail.com>
2016-09-11 10:40:05 +0200

    output raw data of connected clients

--- web/main.dart
+++ web/main.dart
@@ -1,31 +1,26 @@
 // Copyright (c) 2016, Dennis Kaselow. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'dart:async';
+import 'dart:convert';
 import 'dart:html';
+import 'dart:math';
 
-void main() {
-  window.onDeviceOrientation.listen((event) {
-    querySelector('#alpha').text = '${event.alpha}';
-    querySelector('#beta').text = '${event.beta}';
-    querySelector('#gamma').text = '${event.gamma}';
-    querySelector('#absolute').text = '${event.absolute}';
-  });
+main() async {
 
-  window.onDeviceMotion.listen((event){
-    var acc = event.acceleration;
-    var accGrav = event.accelerationIncludingGravity;
-    var rotationRate = event.rotationRate;
-    var interval = event.interval;
-    querySelector('#x').text = '${acc.x}, grav: ${accGrav.x}';
-    querySelector('#y').text = '${acc.y}, grav: ${accGrav.y}';
-    querySelector('#z').text = '${acc.z}, grav: ${accGrav.z}';
-    querySelector('#rotalpha').text = '${rotationRate.alpha}';
-    querySelector('#rotbeta').text = '${rotationRate.beta}';
-    querySelector('#rotgamma').text = '${rotationRate.gamma}';
-    querySelector('#interval').text = '${interval}';
-  });
+  var webSocket = new WebSocket('wss://isowosi.com/ws/s/webstuff');
+  webSocket.onOpen.listen((_) {
+    webSocket.onMessage.listen((event) {
+      querySelector('#alpha').text = event.data;
+    });
+    webSocket.send('test1');
+    webSocket.send('test2');
+    webSocket.send('test3');
+    webSocket.send('test4');
 
-  window.on['compassneedscalibration'].listen((event) {
-    querySelector('#info').text = 'Your compass needs calibrating! Wave your device in a figure-eight motion';
+    window.onBeforeUnload.listen((event) {
+      webSocket.send('cu');
+      webSocket.close();
+    });
   });
 }
