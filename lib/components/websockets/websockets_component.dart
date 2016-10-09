4a37ddc6caa71ed268c1f17bb810cb705cd48cdd
Dennis Kaselow <dennis.kaselow+github@gmail.com>
2016-10-09 20:34:09 +0200

    change background color of clients

--- lib/components/websockets/websockets_component.dart
+++ lib/components/websockets/websockets_component.dart
@@ -1,12 +1,41 @@
 // Copyright (c) 2016, dennis. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:angular2/core.dart';
+import 'package:webstuff/communication_service.dart';
 
 @Component(
     selector: 'websockets',
     styleUrls: const ['../content.css'],
     templateUrl: 'websockets_component.html')
 class WebsocketsComponent {
+  CommunicationService communicationService;
+  int changes = 1;
 
+  WebsocketsComponent(this.communicationService);
+
+  void changeColor() {
+    var random = new Random();
+    communicationService.send(
+        'changeColor',
+        JSON.encode({
+          'h': random.nextInt(360),
+          's': random.nextInt(100),
+          'l': random.nextInt(100)
+        }));
+    changes++;
+  }
+
+  void resetColor() {
+    communicationService.send(
+        'changeColor',
+        JSON.encode({
+          'h': 0,
+          's': 0,
+          'l': 100
+        }));
+  }
 }
